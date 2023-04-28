095a0893eedf669a82a252f7d0a04b79bb7f3580
Matheus Sabino Souza <matheus.souza@ctw.bmwgroup.com>
2023-04-28 15:47:39 +0100

    .

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Link from "next/link";
-import Date from "../components/date";
 import Layout, { siteTitle } from "../components/layout";
 import { getSortedPostsData } from "../lib/posts";
 import utilStyles from "../styles/utils.module.css";
@@ -27,7 +26,6 @@ export default function Home({ allPostsData }) {
               <Link href={`/posts/${id}`}>{title}</Link>
               <br />
               <small className={utilStyles.lightText}>
-                <Date dateString={date} />
               </small>
             </li>
           ))}
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Date from "../../components/date";
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import utilStyles from '../../styles/utils.module.css';
@@ -13,7 +12,6 @@ export default function Post({ postData }) {
     <article>
       <h1 className={utilStyles.headingXl}>{postData.title}</h1>
       <div className={utilStyles.lightText}>
-        <Date dateString={postData.date} />
       </div>
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </article>
